f504eba8f645870df8a3aea3232d930214a76752
mathe <mateus.matheus.lima@gmail.com>
2022-10-06 00:30:33 -0300

    adiciona constantes

---
+++ gerenciamento-sessao-votacao/src/main/java/com/matheuslima/gerenciamentovotacao/service/utils/CpfValidoUtils.java
@@ -0,0 +1,7 @@
+package com.matheuslima.gerenciamentovotacao.service.utils;
+
+public final class CpfValidoUtils {
+
+    public static final String ABLE_TO_VOTE = "ABLE_TO_VOTE";
+    public static final String UNABLE_TO_VOTE = "UNABLE_TO_VOTE";
+}
---
+++ gerenciamento-sessao-votacao/src/main/java/com/matheuslima/gerenciamentovotacao/service/utils/MessageUtils.java
@@ -0,0 +1,9 @@
+package com.matheuslima.gerenciamentovotacao.service.utils;
+
+public final class MessageUtils {
+    public static final String PAUTA_NAO_ENCONTRADA = "Pauta não encontrada. Informe o identificador de uma pauta existente.";
+    public static final String PAUTA_NAO_ATIVA = "Pauta não está ativa!";
+    public static final String STATUS_NULO = "Não foi possível obter seu status. Tente novamente.";
+    public static final String STATUS_UNABLE = "Você não está habilitado para esta votação!";
+    public static final String VOTO_INVALIDO = "Voto inválido. Informe o voto apenas com Sim/Não";
+}
