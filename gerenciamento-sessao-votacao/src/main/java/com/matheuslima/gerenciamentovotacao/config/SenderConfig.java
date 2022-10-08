1041de7142a8e4888b7f5c8ff3674b023d85143d
mathe <mateus.matheus.lima@gmail.com>
2022-10-07 22:53:18 -0300

    adiciona mais configurações para mensageria do rabbitmq

--- gerenciamento-sessao-votacao/src/main/java/com/matheuslima/gerenciamentovotacao/config/SenderConfig.java
+++ gerenciamento-sessao-votacao/src/main/java/com/matheuslima/gerenciamentovotacao/config/SenderConfig.java
@@ -1,17 +1,41 @@
 package com.matheuslima.gerenciamentovotacao.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.amqp.core.Queue;
 
 @Configuration
 public class SenderConfig {
     @Value("${queue.name}")
     private String queueName;
+    @Value("${queue.exchange}")
+    private String exchange;
+    public static final String ROUTING_KEY = "";
 
     @Bean
-    public Queue queue() {
-        return new Queue(queueName, true);
+    public Exchange declareExchange() {
+        return ExchangeBuilder.directExchange(exchange)
+                .durable(true)
+                .build();
+    }
+
+    @Bean
+    public Queue declareQueue() {
+        return QueueBuilder.durable(queueName)
+                .build();
+    }
+
+    @Bean
+    public Binding declareBinding(Exchange exchange, Queue queue) {
+        return BindingBuilder.bind(queue)
+                .to(exchange)
+                .with(ROUTING_KEY)
+                .noargs();
     }
 }
