571cf9422cc32c2f1a7197ecfc587321ea920cfa
mathe <mateus.matheus.lima@gmail.com>
2022-10-07 22:56:12 -0300

    adicionar a conversao do dto de resultado para que possa enviar para fila de consumo do rabbit

--- gerenciamento-sessao-votacao/src/main/java/com/matheuslima/gerenciamentovotacao/service/QueueSenderService.java
+++ gerenciamento-sessao-votacao/src/main/java/com/matheuslima/gerenciamentovotacao/service/QueueSenderService.java
@@ -1,17 +1,25 @@
 package com.matheuslima.gerenciamentovotacao.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.matheuslima.gerenciamentovotacao.service.dto.ResultadoDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.Exchange;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class QueueSenderService {
     private final RabbitTemplate rabbitTemplate;
-    private final Queue queue;
+    private final Exchange exchange;
 
-    public void send(String mensagem) {
-        rabbitTemplate.convertAndSend(this.queue.getName(), mensagem);
+    public void enviarResultadoParaRabbit(ResultadoDTO resultadoDTO){
+        try {
+            String json = new ObjectMapper().writeValueAsString(resultadoDTO);
+            rabbitTemplate.convertAndSend(exchange.getName(), "", json);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
     }
 }
