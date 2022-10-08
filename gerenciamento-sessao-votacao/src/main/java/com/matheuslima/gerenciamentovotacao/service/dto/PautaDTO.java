4c2ada82fdcdc65cfa14bd9e74657b41c6ef3bc8
mathe <mateus.matheus.lima@gmail.com>
2022-10-07 22:50:09 -0300

    adicionar mais validações no de DTO de pauta

--- gerenciamento-sessao-votacao/src/main/java/com/matheuslima/gerenciamentovotacao/service/dto/PautaDTO.java
+++ gerenciamento-sessao-votacao/src/main/java/com/matheuslima/gerenciamentovotacao/service/dto/PautaDTO.java
@@ -4,13 +4,15 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
 public class PautaDTO {
 
-    @NotNull
+    @NotNull(message = "Campo 'título' é obrigatório")
+    @Size(max = 100)
     private String titulo;
-    @NotNull
+    @Size(max = 255)
     private String descricao;
 }
