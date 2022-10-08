c9632d5e3766f84de1d2dd3e26b8836cb4ecdd80
mathe <mateus.matheus.lima@gmail.com>
2022-10-07 22:58:06 -0300

    adiciona novas mensagens padrões

--- gerenciamento-sessao-votacao/src/main/java/com/matheuslima/gerenciamentovotacao/service/utils/MessageUtils.java
+++ gerenciamento-sessao-votacao/src/main/java/com/matheuslima/gerenciamentovotacao/service/utils/MessageUtils.java
@@ -6,4 +6,7 @@ public final class MessageUtils {
     public static final String STATUS_NULO = "Não foi possível obter seu status. Tente novamente.";
     public static final String STATUS_UNABLE = "Você não está habilitado para esta votação!";
     public static final String VOTO_INVALIDO = "Voto inválido. Informe o voto apenas com Sim/Não";
+    public static final String VOTO_DUPLICIDADE = "Voto já foi realizado nesta pauta.";
+    public static final String PAUTA_ENVIADA = "A Pauta de número {0} já foi enviada. Cadastre uma nova pauta para habilitar uma nova sessão.";
+    public static final String RESULTADO_NAO_ENCONTRADO = "O Resultado não foi encontrado.";
 }
